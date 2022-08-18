a91306028b1564a4fda2de8f529105b0ac2a7e35
Brian David <xbriandavid@gmail.com>
2022-08-17 20:28:17 -0700

    init top root tsx file

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Mainframe from './components/Mainframe'
+ReactDOM.render(
+    <Mainframe />, 
+    document.getElementById("app-root")
+)
